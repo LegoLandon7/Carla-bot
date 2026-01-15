4662d46ab246864e64fd608af8a44842022cf03b
LandonLego7 <legolandon25@gmail.com>
2026-01-15 09:20:48 -0500

    presence update

--- init/set-presence.js
+++ init/set-presence.js
@@ -2,7 +2,7 @@ const { ActivityType, MessageFlags } = require('discord.js');
 
 function setPresence(client) {
         client.user.setPresence({
-        activities: [{ name: 'eating a hamburger', type: ActivityType.Playing }], 
+        activities: [{ name: '/help for commands', type: ActivityType.Playing }], 
         status: 'online',
     });
 
